7268a4a503f89b0f9b3de1a5505b48782db1d60d
Robert Brice <rob.a.brice@gmail.comC>
2017-05-08 08:18:23 +0100

    added game logic

--- client/src/components/GameLogic.js
+++ client/src/components/GameLogic.js
@@ -7,19 +7,50 @@ var GameLogic = function (candidates) {
     GameLogic.prototype = {
 
       getRandomCandidate: function(){
-        const randomCandidate = this.candidates[this.getRandomIndex()]
-        return randomCandidate
+        const candidate = this.candidates[this.getRandomIndex()]
+        this.randomCandidate = candidate 
+        return this.randomCandidate
       },
 
       getRandomIndex: function() {
         min = Math.ceil(0);
         max = Math.floor(this.candidates.length - 1);
         return Math.floor(Math.random() * (max - min + 1)) + min;
+      },
+
+      genderGuess: function(gender){
+
+        if (gender === this.randomCandidate.gender) {
+            return "yes! " + gender + " is the right gender"
+          }
+          else
+          {
+              return "no! " + gender + " is the wrong gender"          
+          }
+      },
+
+      hairColourGuess: function(hairColour){
+        if (hairColour === this.randomCandidate.hairColour) {
+          return "yes! " + hairColour + " is the right hair colour"
+        }
+        else
+        {
+            return "no! " + hairColour + " is the wrong hair colour"          
+        }
+      },
+
+      heightGuess: function(height){
+
+        if (height === this.randomCandidate.height) {
+          return "yes! " + height + " is the right height"
+        }
+        else
+        {
+            return "no! " + height + " is the wrong height"          
+        }
+
+
       }
-
-
-
-
 
 
     }
--- client/src/containers/GameContainer.jsx
+++ client/src/containers/GameContainer.jsx
@@ -16,7 +16,11 @@ class GameContainer extends React.Component{
       {name: 'Jo', gender: 'female', hairColour: 'green', height: 4},
       {name: 'Graham', gender: 'male', hairColour: 'yellow', height: 5},
       {name: 'Trixie', gender: 'female', hairColour: 'black', height: 6}
-      ] }
+      ],
+      genderGuessResponse: '',
+      hairColourGuessResponse: '',
+      heightGuessReponse:''
+    }
 
       this.gameLogic = new GameLogic(this.state.candidates)
       this.candidate = this.gameLogic.getRandomCandidate();
@@ -26,19 +30,30 @@ class GameContainer extends React.Component{
 
     setSelectedGender(gender){
       console.log(gender)
+      console.log(this.gameLogic.genderGuess(gender))
+      this.setState({genderGuessResponse: this.gameLogic.genderGuess(gender)})
     }
 
     setSelectedHairColour(colour){
       console.log(colour)
+      this.setState({hairColourGuessResponse: this.gameLogic.hairColourGuess(colour)})
     }
 
     setSelectedHeight(height){
       console.log(height)
+      this.setState({heightGuessResponse: this.gameLogic.heightGuess(height)})
     } 
 
 
     setSelectedCandidate(name){
       console.log(name)
+    }
+
+    resetGuesses(event){
+      console.log('reset guesses')
+      this.setState({genderGuessResponse: ''})
+      this.setState({hairColourGuessResponse: ''})
+      this.setState({heightGuessResponse: ''})
     }
 
     render(){
@@ -53,16 +68,22 @@ class GameContainer extends React.Component{
 
         <div>Guess their gender</div>
         <div><GenderSelector candidates={this.state.candidates} setSelectedGender={this.setSelectedGender.bind(this)}/></div>
+        <div className="guess-response">{this.state.genderGuessResponse}</div>
         
         <div>Guess their hair colour</div>
         <div><ColourSelector candidates={this.state.candidates} setSelectedHairColour={this.setSelectedHairColour.bind(this)}/></div>
-        
+        <div className="guess-response">{this.state.hairColourGuessResponse}</div>
+
+
         <div>Guess their height</div>
-
         <div><HeightSelector candidates={this.state.candidates} setSelectedHeight={this.setSelectedHeight.bind(this)}/></div>
+        <div className="guess-response">{this.state.heightGuessResponse}</div>
 
 
-        <div>Guess who I am thinking of!</div>
+        <div><button onClick={this.resetGuesses.bind(this)}>Reset Guesses</button></div>
+
+
+        <div>Guess who I'm thinking of!</div>
         <div><GuessWhichCandidateSelector candidates={this.state.candidates} setSelectedCandidate={this.setSelectedCandidate.bind(this)}/></div>
 
 
